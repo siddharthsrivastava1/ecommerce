84d2e10b3d424e8e4010db9c365e326cde33b2f2
siddharth <siddharth.srivastava@tothenew.com>
2025-03-19 17:33:41 +0530

    newLetter change

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Title from "../components/Title";
 import { assets } from "../assets/assets";
-import NewsletterBox from "../components/NewsletterBox";
+import NewsLetterBox from "../components/NewsLetterBox";
 
 const About = () => {
   return (
@@ -69,7 +69,7 @@ const About = () => {
         </div>
       </div>
 
-      <NewsletterBox />
+      <NewsLetterBox />
     </div>
   );
 };
